605108d8a9605ad4508295510a248560a2f9cb36
Shivam Singh <shivamsingh4458@gmail.com>
2021-10-08 20:38:01 +0530

    portfolio

---
+++ src/components/About/BackEnd.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiNodejs,
+  DiMongodb,
+  DiMysql,
+} from "react-icons/di";
+import {
+  // SiExpress,
+  SiFirebase,
+} from "react-icons/si";
+
+function BackEnd() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+    
+    </Row>
+  );
+}
+
+export default BackEnd;
+
---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiHeroku,
+} from "react-icons/si";
+import {
+  DiGit,
+} from "react-icons/di";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHeroku />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
