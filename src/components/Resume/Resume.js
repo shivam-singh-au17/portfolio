ce84a11de774220899039933069d58e9693266f7
SHIVAM SINGH <76985787+shivam-singh-au17@users.noreply.github.com>
2026-05-06 18:09:28 +0530

    resume updated

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle2 from "../Particle2";
 import Resumecontent from "./ResumeContent";
-import pdf from "../../Assets/Shivam_Singh_Resume.pdf";
+import pdf from "../../Assets/shivamsingh4458@gmail.com.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 
 function Resume() {
