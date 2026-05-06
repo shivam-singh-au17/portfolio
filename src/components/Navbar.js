a6ff633609358548ea5dc548471607afab85f104
Shivam Singh <shivamsingh4458@gmail.com>
2026-05-06 18:21:50 +0530

    fix: changes for redeployment
    Signed-off-by: Shivam Singh <shivamsingh4458@gmail.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,6 +15,7 @@ function NavBar() {
     updateExpanded(false);
   };
 
+  // console.log("scrollintoview", window);
   function scrollHandler() {
     if (window.scrollY >= 20) {
       updateNavbar(true);
